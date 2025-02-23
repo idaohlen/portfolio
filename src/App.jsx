d7432436dd43b6fb6dd34f85f9b156124a3464b2
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-02-23 20:06:48 +0100

    add iconify/react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
+import { Icon } from "@iconify/react";
 
 function App() {
 
   return (
     <>
       <h1 className='text-4xl'>Ida Öhlén</h1>
-      <p>Fullstack Developer</p>
+      <p className='2xl'>Fullstack Developer</p>
+      <div className='flex flex-row justify-center text-4xl'>
+        <Icon icon='mdi:linkedin'/>
+        <Icon icon='mdi:github'/>
+      </div>
     </>
   )
 }
