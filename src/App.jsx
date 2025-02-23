ba47cad6a9b1d037c9e242585a9e02930397a05a
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-02-23 20:11:41 +0100

    add Motion animation library

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
 import { useState } from 'react'
-import { Icon } from "@iconify/react";
+import { motion } from 'motion/react'
+import { Icon } from '@iconify/react'
 
 function App() {
 
   return (
-    <>
+    <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 2 }}
+  >
       <h1 className='text-4xl'>Ida Öhlén</h1>
       <p className='2xl'>Fullstack Developer</p>
       <div className='flex flex-row justify-center text-4xl'>
         <Icon icon='mdi:linkedin'/>
         <Icon icon='mdi:github'/>
       </div>
-    </>
+    </motion.div>
   )
 }
 
